e178c66fd7e754723e032af330241c71305f907e
Fernando <fernando.guerra@corujasdev.com.br>
2022-04-29 09:36:14 -0300

    App Inicial

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet, ActivityIndicator, Alert, FlatList } from 'react-native';
+import { List } from 'react-native-paper';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [locais, setLocais] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== "granted") {
+          Alert.alert("Insufficient permissions!")
+          return;
+        }
+      }
+
+      let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest, maximumAge: 10000 });
+      buscarLocais(location.coords.latitude, location.coords.longitude)
+      setLoading(false);
+    })();
+  }, []);
+
+  const buscarLocais = (latitude, longitude) => {
+    const url = `http://172.18.208.1:5128/api/Local?Latitude=${latitude}&Longitude=${longitude}&metros=3000`
+
+    fetch(url).then(response => {
+      return response.json();
+    }).then(data => {
+      setLocais(data);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        loading ? <ActivityIndicator size="small" color="#0000ff" /> : <FlatList
+          data={locais}
+          renderItem={({ item }) => (
+            <List.Item
+              title={`${item.nome} - ${item.distancia.toString().split('.', 1)[0]}m`}
+              description={item.endereco}
+            />
+          )} />
+      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
---
+++ README.md
@@ -0,0 +1,5 @@
+#Neste projeto iremos consumir a nossa API desenvolvida em C# utilizando o .Net 6 de forma simples e rápida para listar locais próximos a um ponto
+
+Para saber todo o processo de desenvolvimento da nossa API acesse https://pingback.com/corujasdev/calculando-a-distancia-entre-dois-pontos-com-net-6
+
+Para acessar todo o processo de desenvolvimento do nosso APP acesse 
